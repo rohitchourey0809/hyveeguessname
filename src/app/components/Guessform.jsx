2b604f16b5e8e3f4e8cf616b02f89f14cf3ed44b
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2024-05-22 18:12:37 +0530

    animation added

--- src/app/components/Guessform.jsx
+++ src/app/components/Guessform.jsx
@@ -35,8 +35,8 @@ export default function Guessform() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-100">
-      <div className="w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
-        <h1 className="text-2xl text-cyan-950 font-serif font-bold mb-6 text-center hover:text-pink-600 hover:underline">
+      <div className="animate-wiggle-more w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
+        <h1 className="animate-pulse  text-2xl hover:text-cyan-950 font-serif font-bold mb-6 text-center text-pink-600 hover:underline">
           Guess Name
         </h1>
         <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
@@ -50,15 +50,13 @@ export default function Guessform() {
           />
           <button
             type="submit"
-            className="p-3 hover:text-pink-600 bg-blue-500 text-white rounded-md hover:bg-green-600 transition duration-200"
+            className="animate-bounce   p-3 hover:text-pink-600 bg-blue-500 text-white rounded-md hover:bg-green-600 transition duration-200"
           >
             Guess
           </button>
         </form>
 
-        {data && (
-          <Table data={data}/>
-        )}
+        {data && <Table data={data} />}
       </div>
     </main>
   );
